9e61402202ec51b7a4bd44c6087d101df855cf9c
Josh M <korhadris@hotmail.com>
2017-11-29 22:06:34 -0700

    Adding multiple fan support

--- smartapps/korhadris/smart-hvac-fan.src/smart-hvac-fan.groovy
+++ smartapps/korhadris/smart-hvac-fan.src/smart-hvac-fan.groovy
@@ -27,61 +27,102 @@ definition(
 preferences {
     section("Temperature diffference to turn on fan") {
         input "max_delta", "number", required: true, title: "Max temp difference"
-        input "hysteresis", "number", required: true, title: "Hysteresis"
+        input "hysteresis", "number", required: true, title: "Hysteresis", default: 0
     }
-    section("Thermostat") {
-        input "thermostat", "capability.thermostat", required: true, title: "Thermostat (main temperature and fan control)"
+    section("Fans") {
+        input "thermostat_fans", "capability.thermostat", required: false, title: "Thermostat Fans", multiple: true
+        input "switch_fans", "capability.switch", required: false, title: "Switched Fans", multiple: true
     }
-    section("Temperature sensors to use") {
-    	// input "temp1", "capability.temperatureMeasurement", required: true, title: "Additional Sensor"
-        input "temps", "capability.temperatureMeasurement", required: false, title: "Additional Sensors", multiple: true
+    section("Temperature sensors") {
+        input "temps", "capability.temperatureMeasurement", required: true, title: "Sensors", multiple: true
     }
 }
 
+
 def installed() {
-    log.debug "Installed with settings: ${settings}"
+    // log.debug "Installed with settings: ${settings}"
     initialize()
 }
 
+
 def updated() {
-    log.debug "Updated with settings: ${settings}"
+    // log.debug "Updated with settings: ${settings}"
     unsubscribe()
     initialize()
 }
 
+
 def initialize() {
-    subscribe(thermostat, "temperature", temperatureChangeHandler)
     temps.each() {
         subscribe(it, "temperature", temperatureChangeHandler)
     }
     checkTemperatures()
 }
 
+
 def temperatureChangeHandler(evt) {
     // log.debug "Temperature change: ${evt}"
     checkTemperatures()
 }
 
+
+def fansOn() {
+    // log.debug("fansOn")
+    thermostat_fans.each() {
+        if (it.currentValue("thermostatFanMode") != "on") {
+            // log.debug("Turning thermostat fan on")
+            it.fanOn()
+        }
+    }
+    switch_fans.each() {
+        if (it.currentValue("switch") != "on") {
+            // log.debug("Turning switched fan on")
+            it.on()
+        }
+    }
+}
+
+
+def fansOff() {
+    // log.debug("fansOff")
+    thermostat_fans.each() {
+        if (it.currentValue("thermostatFanMode") != "auto") {
+            // log.debug("Turning thermostat fan auto")
+            it.fanAuto()
+        }
+    }
+    switch_fans.each() {
+        if (it.currentValue("switch") != "off") {
+            // log.debug("Turning switched fan off")
+            it.off()
+        }
+    }
+}
+
+
 def checkTemperatures() {
-    def therm = thermostat.currentValue("temperature")
+    // log.debug "check"
+    //def temp_1 = 0//thermostat.currentValue("temperature")
     def max_diff = 0//temp_diff.abs()
     temps.each() {
-        def temp_diff = it.currentValue("temperature") - therm
-        temp_diff = temp_diff.abs()
-        log.debug "Temp diff: ${temp_diff}"
-        if (temp_diff > max_diff) {
-            max_diff = temp_diff;
-            log.debug "Max diff: ${max_diff}"
+        def temp_1 = it.currentValue("temperature")
+        log.debug "temp_1 ${temp_1}"
+        temps.each() { it2 ->
+            def temp_diff = it2.currentValue("temperature") - temp_1
+            temp_diff = temp_diff.abs()
+            // log.debug "Temp diff: ${temp_diff}"
+            if (temp_diff > max_diff) {
+                max_diff = temp_diff;
+                log.debug "Max diff: ${max_diff}"
+            }
         }
     }
 
     if (max_diff > max_delta) {
-        log.debug "Turn on fan"
-        thermostat.fanOn()
+        fansOn()
     } else if (max_diff < (max_delta - hysteresis)) {
-        log.debug "Turn off fan"
-        thermostat.fanAuto()
+        fansOff()
     } else {
-        log.debug "Leave fan alone"
+        // log.debug "Leave fan alone"
     }
 }
